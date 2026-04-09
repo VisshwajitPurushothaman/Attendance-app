48e7607a1c0d66825d52fb7d015ce2e49fd844d5
VisshwajitPurushothaman <visshwajitpurushothaman2007@gmail.com>
2026-04-09 23:08:31 +0530

    Refactor project structure to unify output directory from 'dist/spa' to 'dist' across configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist/spa",
+    outDir: "dist",
   },
   plugins: [react(), /* expressPlugin() */],
   resolve: {
